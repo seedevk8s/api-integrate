8124e43bca9f13fc8c9edb0d7111d66b1acf18e7
ho jin chu <50796809+seedevk8s@users.noreply.github.com>
2020-09-07 22:04:52 +0900

    Add files via upload

---
+++ public/ReferenceParamEx.java
@@ -0,0 +1,25 @@
+package example04;
+
+public class ReferenceParamEx {
+
+	public static void main(String[] args) {
+		
+		A a = new A();
+		a.data = 77;
+		System.out.println("메서드 호출전 main()내의 값 : " + a.data);
+		//동생한테 "시계가 어딨니?"라고 물어보는 형태라서 값을 읽고 
+		//수정도 할수 있다.
+		//call bv reference(주소의 의한 호출)
+		ReferenceParamEx.dataChange(a);
+		System.out.println("dataChange()호출");
+		System.out.println("메서드 호출후 main()내의 값 : " + a.data);
+	}	
+	//int[], double[], String --> 참조변수형태 호출
+	public static void dataChange(A a) {
+		//a라는 주소값은 매개변수로 넘어왔기 때문에 주소를 공유한다. 
+		//a라는 주소를 공유를 하게 되면 호출한 곳에서 영향을 무조건 받는다.
+		a.data = 44;
+		System.out.println("dataChange()내의 값 : " + a.data);		
+		return;
+	}
+}
